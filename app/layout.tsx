3d8971cac4eb33d9261d41e161d2cadce725dd9f
Tushar Vaghela <tushar.proximity@hotstar.com>
2024-09-17 21:11:31 +0530

    feat: vercel speed insight

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import Script from "next/script";
 import localFont from "next/font/local";
 import "./globals.css";
+import { SpeedInsights } from '@vercel/speed-insights/next';
+
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -39,6 +41,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <SpeedInsights />
       </body>
     </html>
   );
