a40037428b35daa52454a1cfbab14d758bd380bb
Tushar Vaghela <tushar.proximity@hotstar.com>
2024-09-20 12:03:33 +0530

    fix: added root page link in header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,18 +1,22 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 export function Header(){
     return (
         <header className="border-b">
-        <div className="container mx-auto px-4 py-4 flex justify-start items-center space-x-4">
-          <Image
-            width={36}
-            height={36}
-            src='/career.svg'
-            alt='Career Pages Logo'
-            priority
-          />
-          <h1 className="text-2xl font-bold">Career Pages</h1>
-        </div>
+        
+          <Link href="/">
+            <div className="container mx-auto px-4 py-4 flex justify-start items-center space-x-4">
+              <Image
+                width={36}
+                height={36}
+                src='/career.svg'
+                alt='Career Pages Logo'
+                priority
+              />
+              <h1 className="text-2xl font-bold">Career Pages</h1>
+            </div>
+          </Link>
       </header>
     )
 }
